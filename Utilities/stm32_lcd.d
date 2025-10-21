a51bdd4cd2dc2340f4ab818d5743d2ef30b9aac5
LibbyGlieden <142737283+LibbyGlieden@users.noreply.github.com>
2025-10-21 11:33:48 -0400

    Add files via upload

---
+++ Utilities/stm32_lcd.d
@@ -0,0 +1,20 @@
+Utilities/stm32_lcd.o: \
+ /Users/libbyglieden/STM32CubeIDE/workspace_1.19.0/BLE_p2pServer/Utilities/LCD/stm32_lcd.c \
+ /Users/libbyglieden/STM32CubeIDE/workspace_1.19.0/BLE_p2pServer/Utilities/LCD/stm32_lcd.h \
+ /Users/libbyglieden/STM32CubeIDE/workspace_1.19.0/BLE_p2pServer/Utilities/LCD/../Fonts/fonts.h \
+ ../../Drivers/BSP/Components/Common/lcd.h \
+ /Users/libbyglieden/STM32CubeIDE/workspace_1.19.0/BLE_p2pServer/Utilities/LCD/../Fonts/font24.c \
+ /Users/libbyglieden/STM32CubeIDE/workspace_1.19.0/BLE_p2pServer/Utilities/LCD/../Fonts/fonts.h \
+ /Users/libbyglieden/STM32CubeIDE/workspace_1.19.0/BLE_p2pServer/Utilities/LCD/../Fonts/font20.c \
+ /Users/libbyglieden/STM32CubeIDE/workspace_1.19.0/BLE_p2pServer/Utilities/LCD/../Fonts/font16.c \
+ /Users/libbyglieden/STM32CubeIDE/workspace_1.19.0/BLE_p2pServer/Utilities/LCD/../Fonts/font12.c \
+ /Users/libbyglieden/STM32CubeIDE/workspace_1.19.0/BLE_p2pServer/Utilities/LCD/../Fonts/font8.c
+/Users/libbyglieden/STM32CubeIDE/workspace_1.19.0/BLE_p2pServer/Utilities/LCD/stm32_lcd.h:
+/Users/libbyglieden/STM32CubeIDE/workspace_1.19.0/BLE_p2pServer/Utilities/LCD/../Fonts/fonts.h:
+../../Drivers/BSP/Components/Common/lcd.h:
+/Users/libbyglieden/STM32CubeIDE/workspace_1.19.0/BLE_p2pServer/Utilities/LCD/../Fonts/font24.c:
+/Users/libbyglieden/STM32CubeIDE/workspace_1.19.0/BLE_p2pServer/Utilities/LCD/../Fonts/fonts.h:
+/Users/libbyglieden/STM32CubeIDE/workspace_1.19.0/BLE_p2pServer/Utilities/LCD/../Fonts/font20.c:
+/Users/libbyglieden/STM32CubeIDE/workspace_1.19.0/BLE_p2pServer/Utilities/LCD/../Fonts/font16.c:
+/Users/libbyglieden/STM32CubeIDE/workspace_1.19.0/BLE_p2pServer/Utilities/LCD/../Fonts/font12.c:
+/Users/libbyglieden/STM32CubeIDE/workspace_1.19.0/BLE_p2pServer/Utilities/LCD/../Fonts/font8.c:
